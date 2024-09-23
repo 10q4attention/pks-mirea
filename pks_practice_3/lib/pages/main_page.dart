f0fd8203f4bb36aca98751b68c7f0c137e84e53c
10q4attention <10q4attention@mail.ru>
2024-09-23 23:40:31 +0300

    finished

--- pks_practice_3/lib/pages/main_page.dart
+++ pks_practice_3/lib/pages/main_page.dart
@@ -6,8 +6,26 @@ class MainPage extends StatelessWidget {
   MainPage({super.key});
 
   final List<Device> devices = [
-    Device(1, 'Очень хороший телефон', 10, 'Телефоны', 'IPHONE 15 PRO MAX 1TB',
-        1000, 'https://img.mvideo.ru/Big/30074465bb.jpg'),
+    Device(1, 'Ручной пылесос с разными насадками', 10, 'Пылесосы', 'Пылесос Dyson V8',
+        1000, 'https://img.mvideo.ru/Pdb/20088199b.jpg'),
+    Device(2, 'Прекрасный робот-пылесос', 1, 'Пылесосы', 'Xiaomi Robot Vacuum S12 EU',
+        1000, 'https://img.mvideo.ru/Big/400160566bb.jpg'),
+    Device(3, 'Ручной пылесос с разными насадками', 14, 'Пылесосы', 'Samsung VS15A6031R4',
+        1000, 'https://img.mvideo.ru/Big/20082737bb.jpg'),
+    Device(4, 'Прекрасный робот-пылесос', 12, 'Пылесосы', 'Dreame Trouver S10',
+        1000, 'https://img.mvideo.ru/Big/400342322bb.jpg'),
+    Device(5, 'Ручной пылесос с разными насадками', 15, 'Пылесосы', 'Haier HVC400HE',
+        1000, 'https://img.mvideo.ru/Big/400077580bb1.jpg'),
+    Device(6, 'Стильный и вместительный холодильник', 6, 'Холодильники', 'Gorenje NRK6202AXL4',
+        1000, 'https://img.mvideo.ru/Big/20072561bb.jpg'),
+    Device(7, 'Очень хороший телефон', 3, 'Холодильники', 'Холодильник Candy CCRN 6180S ',
+        1000, 'https://img.mvideo.ru/Big/20070161bb.jpg'),
+    Device(8, 'Очень хороший телефон', 19, 'Холодильники', 'Холодильник Hisense RS840N4AIF',
+        1000, 'https://img.mvideo.ru/Pdb/400161108b.jpg'),
+    Device(9, 'Очень хороший телефон', 11, 'Холодильники', 'Холодильник Hotpoint HT 9201I W O3',
+        1000, 'https://img.mvideo.ru/Big/400258219bb.jpg'),
+    Device(10, 'Очень хороший телефон', 1, 'Холодильники', 'Холодильник Gorenje NRK6202AC4',
+        1000, 'https://img.mvideo.ru/Big/20072562bb.jpg'),
   ];
 
   @override
