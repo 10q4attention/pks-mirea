6b69abfa76fa64db60b54b58a4214385e77a7849
10q4attention <10q4attention@mail.ru>
2024-10-01 00:54:40 +0300

    4th practice

---
+++ pks_practice_4/lib/components/device_card.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import '../pages/device_page.dart';
+import '../models/device.dart';
+
+class DeviceCard extends StatelessWidget {
+  final Device device;
+
+  const DeviceCard({super.key, required this.device});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: const Color(0xFF424242), 
+      elevation: 4,
+      margin: const EdgeInsets.all(16),
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => DevicePage(device: device),
+            ),
+          );
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.network(
+                device.imageUrl,
+                height: 300,
+                width: double.infinity,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    device.title,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white, 
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    device.category,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey, 
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    '${device.price} ₽',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white, 
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ pks_practice_4/lib/pages/main_page.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import '../models/device.dart';
+import '../components/device_card.dart';
+import '../pages/add_device_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+final List<Device> devices = [
+  Device(1, 'Ручной пылесос с разными насадками', 10, 'Пылесосы',
+      'Пылесос Dyson V8', 55000, 'https://img.mvideo.ru/Pdb/20088199b.jpg'),
+  Device(
+      2,
+      'Прекрасный робот-пылесос',
+      1,
+      'Пылесосы',
+      'Xiaomi Robot Vacuum S12 EU',
+      40000,
+      'https://img.mvideo.ru/Big/400160566bb.jpg'),
+  Device(3, 'Ручной пылесос с разными насадками', 14, 'Пылесосы',
+      'Samsung VS15A6031R4', 56000, 'https://img.mvideo.ru/Big/20082737bb.jpg'),
+  Device(4, 'Прекрасный робот-пылесос', 12, 'Пылесосы', 'Dreame Trouver S10',
+      70000, 'https://img.mvideo.ru/Big/400342322bb.jpg'),
+  Device(5, 'Ручной пылесос с разными насадками', 15, 'Пылесосы',
+      'Haier HVC400HE', 10000, 'https://img.mvideo.ru/Big/400077580bb1.jpg'),
+  Device(6, 'Стильный и вместительный холодильник', 6, 'Холодильники',
+      'Gorenje NRK6202AXL4', 32000, 'https://img.mvideo.ru/Big/20072561bb.jpg'),
+  Device(
+      7,
+      'Очень хороший телефон',
+      3,
+      'Холодильники',
+      'Холодильник Candy CCRN 6180S ',
+      78000,
+      'https://img.mvideo.ru/Big/20070161bb.jpg'),
+  Device(
+      8,
+      'Очень хороший телефон',
+      19,
+      'Холодильники',
+      'Холодильник Hisense RS840N4AIF',
+      98000,
+      'https://img.mvideo.ru/Pdb/400161108b.jpg'),
+  Device(
+      9,
+      'Очень хороший телефон',
+      11,
+      'Холодильники',
+      'Холодильник Hotpoint HT 9201I W O3',
+      123000,
+      'https://img.mvideo.ru/Big/400258219bb.jpg'),
+  Device(
+      10,
+      'Очень хороший телефон',
+      1,
+      'Холодильники',
+      'Холодильник Gorenje NRK6202AC4',
+      555000,
+      'https://img.mvideo.ru/Big/20072562bb.jpg'),
+];
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Товары'),
+      ),
+      backgroundColor: const Color(0xFFF5DEB3),
+      body: ListView.builder(
+        itemCount: devices.length,
+        itemBuilder: (BuildContext context, int index) {
+          return DeviceCard(device: devices[index]);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddDevicePage(onAddDevice: (newItem) {
+                setState(() {
+                  devices.add(newItem);
+                });
+              }),
+            ),
+          );
+        },
+        backgroundColor: Colors.black,
+        foregroundColor: const Color(0xFFF5DEB3),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
